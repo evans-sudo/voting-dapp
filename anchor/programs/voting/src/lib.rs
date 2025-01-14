858ca18f14788321d90ce4caa4a76e9d7806501c
evans-sudo <chiamaevans10@gmail.com>
2025-01-14 06:16:32 +0000

    refactor: rename votingdapp to voting and update dependencies

---
+++ anchor/programs/voting/src/lib.rs
@@ -0,0 +1,13 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod votingdapp {
+    use super::*;
+
+  
+
+}
